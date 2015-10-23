477e103b055b753da3e8803fbb90b6ea250c09f6
Sergeev Eduard <esergeev@ramber-co.ru>
2015-10-23 15:42:43 +0300

    minimum funcs and tests

---
+++ rate/src/main/java/ru/rambler/rate/RateActivity.java
@@ -0,0 +1,13 @@
+package ru.rambler.rate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate);
+    }
+}
