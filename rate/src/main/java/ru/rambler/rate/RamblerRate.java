9f69064985ffdda8d2f0b50e0436e3c2f51904e3
Sergeev Eduard <esergeev@ramber-co.ru>
2015-10-28 16:35:21 +0300

    handle requestCode == Activity.RESULT_CANCELED

--- rate/src/main/java/ru/rambler/rate/RamblerRate.java
+++ rate/src/main/java/ru/rambler/rate/RamblerRate.java
@@ -81,7 +81,7 @@ public class RamblerRate {
             return false;
         }
 
-        if (resultCode == RateActivity.RESULT_CODE_LATER) {
+        if (resultCode == RateActivity.RESULT_CODE_LATER || requestCode == Activity.RESULT_CANCELED) {
             Prefs.newInstance(instance.configuration.getContext()).setInitTimestamp(Utils.eraseTime(System.currentTimeMillis()));
             callback.remindLater();
         } else if (resultCode == RateActivity.RESULT_CODE_CANCEL) {
